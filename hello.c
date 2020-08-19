ff03aaac9e4a84667f3ec774b266d7f8a4f5b11e
Arthritus <nunger7@huskers.unl.edu>
2020-08-19 13:47:18 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Nathan Unger
+ *         nunger7@huskers.unl.edu
+ * Date: 2020/16/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Nathan Unger\n");
+
+  return 0;
+}
